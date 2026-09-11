f489bbbe15bba2ef5447e081c21da985694383f7
Soumadip <39thdecember@gmail.com>
2026-09-11 15:59:47 +0530

    feat: create utils  AppError & asyncHandler

---
+++ server/src/utils/asyncHandler.ts
@@ -0,0 +1,11 @@
+// async-wrapper
+// foward async-exceptions -> global-handler
+import type { Request, Response, NextFunction } from "express";
+
+export function asyncHandler(
+  func: (req: Request, res: Response, next: NextFunction) => Promise<void>,
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    func(req, res, next).catch(next);
+  };
+}
