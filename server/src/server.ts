1fc4c6c5adc160937671b8ace36e9c9f38703d02
Soumadip <39thdecember@gmail.com>
2026-09-12 23:15:46 +0530

    feat: setup server ✅

---
+++ server/src/server.ts
@@ -0,0 +1,49 @@
+import "dotenv/config";
+import express from "express";
+import { connectDB } from "./db";
+import cors from "cors";
+import morgan from "morgan";
+import { ok } from "./utils/envelope";
+import { errorHandler } from "./middlewares/errHandler.middleware";
+import { notFound } from "./middlewares/notFound.middleware";
+
+async function mainEntry() {
+  await connectDB();
+
+  const app = express();
+
+  const corsOrigins = (process.env.CORS_ORIGINS || "http://localhost:3000")
+    .split(",")
+    .map((origin) => origin.trim())
+    .filter(Boolean);
+
+  app.use(
+    cors({
+      origin: corsOrigins,
+      credentials: true,
+    }),
+  );
+
+  app.use(express.json());
+  app.use(morgan("dev"));
+
+  // endpoints
+  app.get("/health", (_req, res) => {
+    res.status(200).json(ok({ message: "Server is healthy & runnning ✅" }));
+  });
+
+  // custom-middlewares
+  app.use(notFound);
+  app.use(errorHandler);
+
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT}`);
+  });
+}
+
+mainEntry().catch((err) => {
+  console.error("🔴 failed to start:", err);
+  process.exit(1);
+});
